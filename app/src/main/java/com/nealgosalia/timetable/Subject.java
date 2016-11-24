842bfdb8310f7863713785e5e41c45d0d6ba0a69
nealgosalia <nealgosalia@gmail.com>
2016-11-25 02:11:49 +0530

    Add initial support for adding Subjects

---
+++ app/src/main/java/com/nealgosalia/timetable/Subject.java
@@ -0,0 +1,35 @@
+package com.nealgosalia.timetable;
+
+import java.util.Comparator;
+
+public class Subject implements Comparable<Subject>{
+
+    private String subjectName;
+
+    public Subject(){
+    }
+
+    public String getSubjectName() {
+        return subjectName;
+    }
+
+    public void setSubjectName(String subjectName) {
+        this.subjectName = subjectName;
+    }
+
+    @Override
+    public int compareTo(Subject o) {
+        return Comparators.NAME.compare(this, o);
+    }
+
+
+    public static class Comparators {
+
+        public static Comparator<Subject> NAME = new Comparator<Subject>() {
+            @Override
+            public int compare(Subject o1, Subject o2) {
+                return o1.subjectName.compareTo(o2.subjectName);
+            }
+        };
+    }
+}
--- app/src/main/java/com/nealgosalia/timetable/SubjectsActivity.java
+++ app/src/main/java/com/nealgosalia/timetable/SubjectsActivity.java
@@ -1,29 +1,82 @@
 package com.nealgosalia.timetable;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.EditText;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 
 public class SubjectsActivity extends AppCompatActivity {
+
+    private List<Subject> subjectsList = new ArrayList<>();
+    private RecyclerView listSubjects;
+    private SubjectsAdapter mSubjectsAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_subjects);
+        listSubjects=(RecyclerView)findViewById(R.id.listSubjects);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        listSubjects = (RecyclerView) findViewById(R.id.listSubjects);
+
+        mSubjectsAdapter = new SubjectsAdapter(subjectsList);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        listSubjects.setLayoutManager(mLayoutManager);
+        listSubjects.setItemAnimator(new DefaultItemAnimator());
+        listSubjects.addItemDecoration(new DividerItemDecoration(this, LinearLayoutManager.VERTICAL));
+        listSubjects.setAdapter(mSubjectsAdapter);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                showSubjectDialog();
             }
         });
     }
 
+    public void showSubjectDialog() {
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = this.getLayoutInflater();
+        final View dialogView = inflater.inflate(R.layout.dialog_add_subject, null);
+        dialogBuilder.setView(dialogView);
+
+        final EditText newSubjectName = (EditText) dialogView.findViewById(R.id.newSubjectName);
+
+        dialogBuilder.setTitle("Subject");
+        dialogBuilder.setMessage("Enter subject name");
+        dialogBuilder.setPositiveButton("Add", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                Subject subject=new Subject();
+                subject.setSubjectName(newSubjectName.getText().toString().trim());
+                subjectsList.add(subject);
+                Collections.sort(subjectsList,Subject.Comparators.NAME);
+                mSubjectsAdapter.notifyDataSetChanged();
+            }
+        });
+        dialogBuilder.setNegativeButton("Cancel", null);
+        AlertDialog dialog = dialogBuilder.create();
+        dialog.show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
 }
