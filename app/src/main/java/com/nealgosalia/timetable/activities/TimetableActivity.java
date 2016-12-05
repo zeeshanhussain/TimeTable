9d310cdc5f77ca4b648201f2f620ef9364a36b71
nealgosalia <nealgosalia@gmail.com>
2016-12-05 18:09:46 +0530

    Fix FAB ID and add OnClickListener

--- app/src/main/java/com/nealgosalia/timetable/activities/TimetableActivity.java
+++ app/src/main/java/com/nealgosalia/timetable/activities/TimetableActivity.java
@@ -1,9 +1,11 @@
 package com.nealgosalia.timetable.activities;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.nealgosalia.timetable.R;
 import com.nealgosalia.timetable.adapters.SimpleFragmentPagerAdapter;
@@ -19,6 +21,12 @@ public class TimetableActivity extends AppCompatActivity {
         viewPager.setAdapter(new SimpleFragmentPagerAdapter(getSupportFragmentManager(),TimetableActivity.this));
         TabLayout tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
         tabLayout.setupWithViewPager(viewPager);
-
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fabTimeTable);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
     }
 }
