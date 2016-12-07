0b00bf0b88dd9ae37f7b9e5c04536cbb07820642
nealgosalia <nealgosalia@gmail.com>
2016-12-08 02:25:11 +0530

    Minor fixes

--- app/src/main/java/com/nealgosalia/timetable/activities/SubjectsActivity.java
+++ app/src/main/java/com/nealgosalia/timetable/activities/SubjectsActivity.java
@@ -37,7 +37,6 @@ public class SubjectsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_subjects);
-        //getSupportActionBar().setElevation(0);
         listSubjects=(RecyclerView)findViewById(R.id.listSubjects);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
