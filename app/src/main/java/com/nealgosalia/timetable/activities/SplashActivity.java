7cdca2f3f37504aaee04b8f4ed6b12547b044400
zeeshanhussain <zeeshanhussain12@gmail.com>
2016-12-11 20:55:50 +0530

    Use splash in fullscreen

--- app/src/main/java/com/nealgosalia/timetable/activities/SplashActivity.java
+++ app/src/main/java/com/nealgosalia/timetable/activities/SplashActivity.java
@@ -3,6 +3,8 @@ package com.nealgosalia.timetable.activities;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.nealgosalia.timetable.MainActivity;
 import com.nealgosalia.timetable.R;
@@ -10,6 +12,9 @@ import com.nealgosalia.timetable.R;
 public class SplashActivity extends Activity {
 
     protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash);
         Thread thread = new Thread() {
