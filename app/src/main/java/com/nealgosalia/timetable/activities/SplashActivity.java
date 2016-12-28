fbc99437639f73dc0f3392fed71cf397868f81b7
zeeshanhussain <zeeshanhussain12@gmail.com>
2016-12-28 15:27:25 +0530

    Fix splash

--- app/src/main/java/com/nealgosalia/timetable/activities/SplashActivity.java
+++ app/src/main/java/com/nealgosalia/timetable/activities/SplashActivity.java
@@ -5,11 +5,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
+import android.os.Handler;
 
 import com.nealgosalia.timetable.MainActivity;
 import com.nealgosalia.timetable.R;
 
 public class SplashActivity extends Activity {
+    private Runnable runnable;
+    private Handler handler;
 
     protected void onCreate(Bundle savedInstanceState) {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
@@ -17,24 +20,27 @@ public class SplashActivity extends Activity {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash);
-        Thread thread = new Thread() {
+        initialize();
+    }
+    private void initialize()
+    {
+        handler = new Handler();
+        runnable = new Runnable()
+        {
             @Override
-            public void run() {
-                try {
-                    sleep(1000);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                } finally {
-                    Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+            public void run()
+            {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
                     startActivity(intent);
-                }
             }
         };
-        thread.start();
+        handler.postDelayed(runnable, 1000);
     }
 
     @Override
     public void onBackPressed() {
         finish();
+        handler.removeCallbacks(runnable);
     }
+
 }
